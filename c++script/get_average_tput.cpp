9c843dc59d2fc9e865bc8cfa165083b9b26216cb
zzunny97 <zzunny97@gmail.com>
2021-01-10 17:50:06 +0900

    .

--- c++script/get_average_tput.cpp
+++ c++script/get_average_tput.cpp
@@ -11,8 +11,8 @@ int main(int argc, char** argv) {
 	int arr[1000];
 	int time, seek;
 	memset(arr, -1, sizeof(arr));
-	for(int i=1; i<=10; i++) {
-		string input_file = "/home/velox/git/blktrace/eternity" + to_string(i) + "/seekwatcher_tput";
+	for(int i=3; i<=10; i++) {
+		string input_file = "/home/velox4/git/blktrace/eternity" + to_string(i) + "/seekwatcher_tput";
 		ifstream in(input_file);
 		if(in.is_open()) {
 			while(!in.eof()) {
